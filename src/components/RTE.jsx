364ca71084897f2bc5ae9542e9192b5e9a54516b
Kavya Mittal <mister1729x@gmail.com>
2025-01-31 13:23:41 +0530

    ok

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                        apiKey={conf.appwriteApiKey}
+                        apiKey='tnr7d2xleel5c3jsy0t01t5fono94zbjuzmcnjtnc73zesf1'
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
